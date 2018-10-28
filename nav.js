499c96157a8117e7a400fb5bad9554f0dbae2729
Razdran <anton.razvan1997@gmail.com>
2018-10-28 03:16:39 +0300

    SideNav + header 1.3

---
+++ nav.js
@@ -0,0 +1,8 @@
+function openNav(){
+    document.getElementById("sideNav").style.width="250px";
+    document.getElementById("container").style.marginLeft="250px";
+}
+function closeNav(){
+    document.getElementById("sideNav").style.width="0";
+    document.getElementById("container").style.marginLeft="0";
+}
