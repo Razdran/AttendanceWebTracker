614a4346a830b11fb831c17650409af8921a02d7
martflorin <mart.florin@yahoo.com>
2018-11-01 17:46:34 +0200

    css+html validator

--- collapsible.js
+++ collapsible.js
@@ -3,11 +3,7 @@ var i;
 
 for (i = 0; i < collapsed.length; i++) {
     collapsed[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
         this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
         var content = this.nextElementSibling;
         if (content.style.display === "block") {
             content.style.display = "none";
