e7c3eaa17d29910bff15203a2163d5f4d5dc2412
constantinstefan <cosminstefan521@gmail.com>
2019-02-09 16:59:28 +0200

    next step

---
+++ SAMPLE/filter/filterData.js
@@ -0,0 +1,332 @@
+var dataBase=[
+  {
+    "active": 1,
+    "maxPrezente": 20,
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "prezente": 20,
+    "sessionCode": "carapace",
+    "titlu": "test2",
+    "key": "-LX2vwvnUyl133pJTmmc"
+  },
+  {
+    "active": 1,
+    "maxPrezente": 30,
+    "organizatorName": "Gavrilut",
+    "organizatorUID": "uid_gavrilut",
+    "prezente": 0,
+    "sessionCode": "poo",
+    "titlu": "POO",
+    "key": "-LX2w77zeX1PrlOPjrRk"
+  },
+  {
+    "active": 1,
+    "maxPrezente": 30,
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "prezente": 0,
+    "sessionCode": "test7",
+    "titlu": "Test7",
+    "key": "-LX2wY7G6u90qJP0OjJD"
+  },
+  {
+    "active": 0,
+    "maxPrezente": 30,
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "prezente": 0,
+    "sessionCode": "test8",
+    "titlu": "Test8",
+    "key": "-LX2wY7K0eeH8TU-Bl5T"
+  },
+  {
+    "active": 1,
+    "maxPrezente": 30,
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "participants": [
+      {
+        "feedback": "smecher",
+        "grade": "12",
+        "id": "yARRnkFD9KQpeakT4Jth1Vimmur2",
+        "name": "cristian stefan"
+      }
+    ],
+    "prezente": 1,
+    "sessionCode": "test9",
+    "titlu": "Test9",
+    "key": "-LX2wY7Oib10hCMsb806"
+  },
+  {
+    "active": 1,
+    "maxPrezente": 30,
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "prezente": 0,
+    "sessionCode": "test10",
+    "titlu": "Test10",
+    "key": "-LX2wepkl-NEVwY_Cs8N"
+  },
+  {
+    "active": 1,
+    "maxPrezente": 30,
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "prezente": 0,
+    "sessionCode": "test11",
+    "titlu": "Test11",
+    "key": "-LX2wepouY2rSglGHcXA"
+  },
+  {
+    "active": 1,
+    "maxPrezente": "5",
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "participants": [
+      {
+        "feedback": "200",
+        "grade": "11",
+        "id": "yARRnkFD9KQpeakT4Jth1Vimmur2",
+        "name": "cristian stefan"
+      }
+    ],
+    "prezente": 1,
+    "sessionCode": "ioana",
+    "titlu": "ioana",
+    "key": "-LXGER5aLViZL1xfd9l-"
+  },
+  {
+    "active": 1,
+    "maxPrezente": "5",
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "participants": [
+      {
+        "feedback": "200",
+        "grade": "11",
+        "id": "yARRnkFD9KQpeakT4Jth1Vimmur2",
+        "name": "cristian stefan"
+      }
+    ],
+    "prezente": 1,
+    "sessionCode": "ioana",
+    "titlu": "ioana",
+    "key": "-LXGESAlhgkfjCC-G7ct"
+  },
+  {
+    "active": 1,
+    "maxPrezente": "2",
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "participants": [
+      {
+        "feedback": "200",
+        "grade": "11",
+        "id": "yARRnkFD9KQpeakT4Jth1Vimmur2",
+        "name": "cristian stefan"
+      }
+    ],
+    "prezente": 1,
+    "sessionCode": "aa",
+    "titlu": "aa",
+    "key": "-LXGEqi_9kbPldD9Mf9B"
+  },
+  {
+    "active": 1,
+    "maxPrezente": "4",
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "participants": [
+      {
+        "feedback": "200",
+        "grade": "11",
+        "id": "yARRnkFD9KQpeakT4Jth1Vimmur2",
+        "name": "cristian stefan"
+      }
+    ],
+    "prezente": 1,
+    "sessionCode": "aa",
+    "titlu": "aa",
+    "key": "-LXGFB11O8AUMfYVWx4d"
+  },
+  {
+    "active": 1,
+    "maxPrezente": "5",
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "participants": [
+      {
+        "feedback": "zzc",
+        "grade": "10",
+        "id": "yARRnkFD9KQpeakT4Jth1Vimmur2",
+        "name": "cristian stefan"
+      }
+    ],
+    "prezente": 1,
+    "sessionCode": "aa",
+    "titlu": "aa",
+    "key": "-LXGFiDvN9I8BLIrFtKO"
+  },
+  {
+    "active": 1,
+    "maxPrezente": "20",
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "participants": [
+      {
+        "feedback": "zzc",
+        "grade": "10",
+        "id": "yARRnkFD9KQpeakT4Jth1Vimmur2",
+        "name": "cristian stefan"
+      }
+    ],
+    "prezente": 1,
+    "sessionCode": "lab3",
+    "titlu": "lab 3",
+    "key": "-LXGTzU68ZaBj3YdOKlX"
+  },
+  {
+    "active": 1,
+    "maxPrezente": "10",
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "participants": [
+      {
+        "feedback": "smec",
+        "grade": "12",
+        "id": "yARRnkFD9KQpeakT4Jth1Vimmur2",
+        "name": "cristian stefan"
+      }
+    ],
+    "prezente": 1,
+    "sessionCode": "lab4",
+    "titlu": "lab4",
+    "key": "-LXGgTZP9qxQIDwZQWII"
+  },
+  {
+    "active": 1,
+    "maxPrezente": "5",
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "participants": [
+      {
+        "feedback": "sme",
+        "grade": "12",
+        "id": "yARRnkFD9KQpeakT4Jth1Vimmur2",
+        "name": "cristian stefan"
+      }
+    ],
+    "prezente": 1,
+    "sessionCode": "5",
+    "titlu": "lab5",
+    "key": "-LXGghE3lfNjS3ekNi5K"
+  },
+  {
+    "active": 1,
+    "maxPrezente": "6",
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "participants": [
+      {
+        "feedback": "smec",
+        "grade": "12",
+        "id": "yARRnkFD9KQpeakT4Jth1Vimmur2",
+        "name": "cristian stefan"
+      }
+    ],
+    "prezente": 1,
+    "sessionCode": "6",
+    "titlu": "lab6",
+    "key": "-LXGgmI-gnk_ZrcCd5oW"
+  },
+  {
+    "active": 1,
+    "maxPrezente": "8",
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "participants": [
+      {
+        "feedback": "smemmm",
+        "grade": "12",
+        "id": "yARRnkFD9KQpeakT4Jth1Vimmur2",
+        "name": "cristian stefan"
+      }
+    ],
+    "prezente": 1,
+    "sessionCode": "8",
+    "titlu": "8",
+    "key": "-LXGhoG3lbHId7jq_h_I"
+  },
+  {
+    "active": 1,
+    "maxPrezente": "20",
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "participants": [
+      {
+        "feedback": "ia si mai invata",
+        "grade": "8",
+        "id": "yARRnkFD9KQpeakT4Jth1Vimmur2",
+        "name": "cristian stefan"
+      }
+    ],
+    "prezente": 1,
+    "sessionCode": "david",
+    "titlu": "lab lui david",
+    "key": "-LXKYW3Sn6PJHnnba8zv"
+  },
+  {
+    "active": 1,
+    "maxPrezente": "10",
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "prezente": 0,
+    "sessionCode": "serban",
+    "titlu": "serban",
+    "key": "-LXQjCGFovwttXB4kd7Y"
+  },
+  {
+    "active": 0,
+    "maxPrezente": 20,
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "prezente": 0,
+    "sessionCode": "querty",
+    "titlu": "mate",
+    "key": "-LXolG9rpaDYxA7GttMi"
+  },
+  {
+    "active": 1,
+    "maxPrezente": "10",
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "participants": [
+      {
+        "feedback": "Please wait for review",
+        "grade": 0,
+        "id": "yARRnkFD9KQpeakT4Jth1Vimmur2",
+        "name": "cristian stefan",
+        "time": 1549224882174
+      }
+    ],
+    "prezente": 1,
+    "sessionCode": "lab44",
+    "titlu": "lab44",
+    "key": "-LXoofujp3p4-towogoP"
+  },
+  {
+    "active": 1,
+    "maxPrezente": "",
+    "organizatorName": "cosmin stefan",
+    "organizatorUID": "dCJ8S4gZk1b9zffvWOHB03qWkKr2",
+    "prezente": 0,
+    "sessionCode": "5hjKje",
+    "titlu": "",
+    "key": "-LYE9YNhTIZwOM227B1l"
+  }
+]
+function do()
+{
+	
+	
+}
